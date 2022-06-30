ed4771c47956f924929f9c44f6793e2e73209fab
loehur <47594852+loehur@users.noreply.github.com>
2022-06-30 21:56:13 +0700

    rekap gaji karyawan2

--- app/Controllers/Rekap.php
+++ app/Controllers/Rekap.php
@@ -59,9 +59,9 @@ class Rekap extends Controller
       $where = $this->wCabang . " AND jenis_transaksi = 4 AND status_mutasi = 3 AND insertTime LIKE '%" . $today . "%' GROUP BY note_primary";
       $kas_keluar = $this->model('M_DB_1')->get_cols_where("kas", $cols, $where, 1);
 
-      //GAJI KARYAWAN//
+      //GAJI KARYAWAN
       $cols = "sum(jumlah) as total";
-      $where = $this->wLaundry . " AND tipe = 1 AND tgl = '" . $today . "'";
+      $where = $this->wCabang . " AND tipe = 1 AND tgl = '" . $today . "'";
       $gaji = $this->model('M_DB_1')->get_cols_where("gaji_result", $cols, $where, 0)['total'];
 
       $this->view($viewData, [
