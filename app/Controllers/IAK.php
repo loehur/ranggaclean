86ffa323aa0a6b733436facbc6df9acce6d6dd00
loehur <loehur@gmail.com>
2022-07-28 15:40:38 +0700

    cb5

--- app/Controllers/IAK.php
+++ app/Controllers/IAK.php
@@ -5,8 +5,9 @@ class IAK extends Controller
    public function callBack()
    {
       $rawRequestInput = file_get_contents("php://input");
-      // Baris ini melakukan format input mentah menjadi array asosiatif
       $arrRequestInput = json_decode($rawRequestInput, true);
+      echo "<pre>";
       print_r($arrRequestInput);
+      echo "</pre>";
    }
 }
